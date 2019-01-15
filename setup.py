9cb6c4b860827e0bdd1f49a1ae351c1963567267
Mitchell Stokes <mogurijin@gmail.com>
2019-01-14 20:14:21 -0800

    setup.py: Add build_all command
    This will handle building all dists (sdist and all platform wheels).
    This includes grabbing the appropriate Blender download for each
    platform.
    
    closes #3

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import os
+import shutil
 
 from setuptools import setup, Command
 from setuptools.command.build_py import build_py as _build_py
 from setuptools.command.develop import develop as _develop
+from setuptools.command.sdist import sdist as _sdist
 from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 from blend2bam import blenderutils
@@ -37,10 +39,62 @@ class build_blender(Command):
             self.platform = bdist_wheel_cmd.get_tag()[2]
 
     def run(self):
-        import shutil
         shutil.rmtree(blenderutils.get_blender_dir(), ignore_errors=True)
         print('Getting Blender for', self.platform)
         blenderutils.download_blender(self.platform)
+
+
+class sdist(_sdist):
+    def run(self):
+        shutil.rmtree(blenderutils.get_blender_dir(), ignore_errors=True)
+        _sdist.run(self)
+
+
+class build_all(Command):
+    description = 'Build binary wheels for all supported platforms'
+    user_options = []
+
+    supported_platforms = [
+        'linux_x86_64',
+        'linux_i386',
+        'win_amd64',
+        'win32',
+    ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        setup_py_args = [
+            'python',
+            'setup.py',
+        ]
+
+        build_sdist_args = setup_py_args + [
+            'sdist',
+        ]
+        print('Building sdist')
+        subprocess.check_call(build_sdist_args)
+
+        for plat in self.supported_platforms:
+            shutil.rmtree(os.path.join(os.path.dirname(__file__), 'build'), ignore_errors=True)
+
+            print('Building binary wheel for', plat)
+            build_blender_args = setup_py_args + [
+                'build_blender',
+                '--platform={}'.format(plat),
+            ]
+            subprocess.check_call(build_blender_args)
+
+            bdist_wheel_args = setup_py_args + [
+                'bdist_wheel',
+                '--plat-name={}'.format(plat),
+            ]
+            subprocess.check_call(bdist_wheel_args)
 
 
 class build_py(_build_py):
@@ -100,7 +154,9 @@ setup(
     },
     cmdclass={
         'build_blender': build_blender,
+        'sdist': sdist,
         'bdist_wheel': bdist_wheel,
+        'build_all': build_all,
         'build_py': build_py,
         'develop': develop,
     },
