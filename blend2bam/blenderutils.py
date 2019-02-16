6d2c194dbcfe2c1977728cbc959ccee9aa03a415
Mitchell Stokes <mogurijin@gmail.com>
2019-02-16 12:27:34 -0800

    blenderutils: Send stdout to DEVNULL when running blender

--- blend2bam/blenderutils.py
+++ blend2bam/blenderutils.py
@@ -4,7 +4,7 @@ import subprocess
 
 def run_blender(args, blenderdir=''):
     os.path.join(blenderdir, 'blender')
-    subprocess.check_call(['blender', '--background'] + args)
+    subprocess.check_call(['blender', '--background'] + args, stdout=subprocess.DEVNULL)
 
 
 def run_blender_script(script, args, blenderdir=''):
