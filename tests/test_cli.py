21f9a22e8b753e10ac4f63075ce2078afbc617b3
Mitchell Stokes <mogurijin@gmail.com>
2019-12-15 14:34:12 -0800

    test_cli: Reduce duplicate code for arguments testing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,19 +10,29 @@ from blend2bam import blenderutils
 
 TESTDIR = os.path.dirname(os.path.abspath(__file__))
 SRCDIR = os.path.join(TESTDIR, 'assets')
+TESTBLEND = os.path.join(SRCDIR, 'test.blend')
 
 
-def test_cli_single(tmpdir):
+def run_cli_test(tmpdir, src=TESTBLEND, extra_args=None):
+    dst = os.path.join(tmpdir, 'output.bam')
     args = [
-        'python',
-        os.path.join(SRCDIR, 'test.blend'),
-        os.path.join(tmpdir, 'output.bam'),
+        'python'
+    ]
+    if extra_args:
+        args += extra_args
+    args += [
+        src, dst
     ]
     sys.argv = args
     blend2bam.cli.main()
     assert os.path.exists(tmpdir)
     assert not os.path.exists(os.path.join(tmpdir, 'test.gltf'))
     assert os.path.exists(os.path.join(tmpdir, 'output.bam'))
+
+
+def test_cli_single(tmpdir):
+    run_cli_test(tmpdir)
+    assert not os.path.exists(os.path.join(tmpdir, 'test.gltf'))
 
 
 def test_cli_relative(tmpdir):
@@ -79,53 +89,25 @@ def test_cli_many_to_dir(tmpdir):
     assert os.path.exists(os.path.join(tmpdir, 'test2.bam'))
 
 def test_cli_physics_builtin(tmpdir):
-    args = [
-        'python',
-        os.path.join(SRCDIR, 'physics.blend'),
-        os.path.join(tmpdir, 'output.bam'),
+    run_cli_test(tmpdir, extra_args=[
         '--physics-engine', 'builtin'
-    ]
-    sys.argv = args
-    blend2bam.cli.main()
-    assert os.path.exists(tmpdir)
-    assert not os.path.exists(os.path.join(tmpdir, 'test.gltf'))
-    assert os.path.exists(os.path.join(tmpdir, 'output.bam'))
+    ])
 
 def test_cli_physics_bullet(tmpdir):
-    args = [
-        'python',
-        os.path.join(SRCDIR, 'physics.blend'),
-        os.path.join(tmpdir, 'output.bam'),
+    run_cli_test(tmpdir, extra_args=[
         '--physics-engine', 'bullet'
-    ]
-    sys.argv = args
-    blend2bam.cli.main()
-    assert os.path.exists(tmpdir)
-    assert not os.path.exists(os.path.join(tmpdir, 'test.gltf'))
-    assert os.path.exists(os.path.join(tmpdir, 'output.bam'))
+    ])
 
 def test_cli_pipeline_egg(tmpdir):
     if blenderutils.is_blender_28():
         pytest.skip('EGG pipeline not supported with blender 2.8')
-    args = [
-        'python',
-        os.path.join(SRCDIR, 'physics.blend'),
-        os.path.join(tmpdir, 'output.bam'),
+    run_cli_test(tmpdir, extra_args=[
         '--pipeline', 'egg'
-    ]
-    sys.argv = args
-    blend2bam.cli.main()
-    assert os.path.exists(tmpdir)
+    ])
     assert not os.path.exists(os.path.join(tmpdir, 'test.egg'))
-    assert os.path.exists(os.path.join(tmpdir, 'output.bam'))
 
 def test_cli_blender_dir(tmpdir):
-    args = [
-        'python',
-        '--blender-dir', 'tests',
-        os.path.join(SRCDIR, 'test.blend'),
-        os.path.join(tmpdir, 'output.bam'),
-    ]
-    sys.argv = args
     with pytest.raises(FileNotFoundError):
-        blend2bam.cli.main()
+        run_cli_test(tmpdir, extra_args=[
+            '--blender-dir', 'tests',
+        ])
