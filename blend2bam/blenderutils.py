a404b63b465de118b60a24e866c331eb6973ded7
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-08-25 20:52:41 -0400

    Add support for Steam version of Blender

--- blend2bam/blenderutils.py
+++ blend2bam/blenderutils.py
@@ -3,8 +3,17 @@ import subprocess
 
 
 def run_blender(args, blenderdir=''):
-    binpath = os.path.join(blenderdir, 'blender')
-    subprocess.check_call([binpath, '--background'] + args, stdout=None)#subprocess.DEVNULL)
+    if blenderdir == '' and blender_exists(blenderdir): # Blender on PATH, continue with defaults
+        binpath = os.path.join(blenderdir, 'blender')
+        subprocess.check_call([binpath, '--background'] + args, stdout=None)#subprocess.DEVNULL)
+    elif blenderdir == '' and not blender_exists(blenderdir): # Attempt to find Blender on Steam
+        blenderdir = 'C:\Program Files (x86)\Steam\steamapps\common\Blender'
+        if blender_exists(blenderdir): # Blender exists on Steam! Use that path
+            binpath = os.path.join(blenderdir, 'blender')
+            subprocess.check_call([binpath, '--background'] + args, stdout=None)#subprocess.DEVNULL)
+        else: # Couldn't find Blender on Steam
+            print('blend2bam couldn\'t find Blender on PATH or Steam. If you are using Steam, you may have installed to a directory different from the defaults\nIf you are using Blender Launcher, use --blender-path <path to blender>.')
+            
 
 
 def run_blender_script(script, args, blenderdir=''):
