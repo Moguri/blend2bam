Moguri/blend2bam

commit 5241a16c5809ce87447665b44ee3c849f0e147e1

Error if legacy materials are requested but are unsupported

blend2bam/gltf2bam/__init__.py
@@ -18,12 +18,13 @@ class ConverterGltf2Bam(ConverterBase):
             'textures': self.settings.textures,
         }
 
-        gltf2bam_version = [int(i) for i in gltf.__version__.split('.')]
-        if gltf2bam_version[1] >= 9:
-            gltf_settings['legacy_materials'] = (
-                blenderutils.is_blender_28()
-                and self.settings.material_mode == 'legacy'
-            )
+        if blenderutils.is_blender_28() and self.settings.material_mode == 'legacy':
+            gltf2bam_version = [int(i) for i in gltf.__version__.split('.')]
+            if gltf2bam_version[0] == 0 and gltf2bam_version[1] < 9:
+                raise RuntimeError(
+                    'panda3d-gltf >= 0.9 is required to use legacy material-mode'
+                    ' with Blender 2.80+'
+                )
         self.gltf_settings = gltf.GltfSettings(**gltf_settings)
 
     def convert_single(self, src, dst):

commit 2e6ba12f8f11790a109091230589b56b092f4d07

Fix gltf2bam not using blender_dir
This is a regression in 5241a16c5809ce87447665b44ee3c849f0e147e1 that prevents blend2bam from working on Windows.

blend2bam/gltf2bam/__init__.py
@@ -18,7 +18,7 @@ class ConverterGltf2Bam(ConverterBase):
             'textures': self.settings.textures,
         }
 
-        if blenderutils.is_blender_28() and self.settings.material_mode == 'legacy':
+        if blenderutils.is_blender_28(settings.blender_dir) and self.settings.material_mode == 'legacy':
             gltf2bam_version = [int(i) for i in gltf.__version__.split('.')]
             if gltf2bam_version[0] == 0 and gltf2bam_version[1] < 9:
                 raise RuntimeError(