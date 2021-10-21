d99713c992a2d525a96501c64d5e0cda2528e416
Mitchell Stokes <mogurijin@gmail.com>
2021-10-20 21:09:45 -0700

    setup.py: Explicitly list Python 3.9 as supported

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='panda3d gamedev',
     url='https://github.com/Moguri/panda3d-blend2bam',
